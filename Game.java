4451c403c4dde762f1b9cec2f4fb03bd4632a4eb
Leah Levensailor <llevensailor@smith.edu>
2024-12-09 14:32:45 -0500

    forgot about message

--- Game.java
+++ Game.java
@@ -21,11 +21,17 @@ public class Game {
                 if(response.contains("move")){
                     ClueMap.move(); 
                     System.out.println("This one works");  
+        
                 }
                 else if(response.contains("look around")){
                     //room.lookAround(); 
                     System.out.println("This one too");
                 }
+                else if(response.contains("show options")){
+                    ClueMap.showOptions(); 
+                }
+             
+
                 else if(response.contains("Mr. Green") && response.contains("candlestick") && response.contains("dining room")){
                     System.out.println("You have figured it out! Congratulations!");
                     stillPlaying = false; }
