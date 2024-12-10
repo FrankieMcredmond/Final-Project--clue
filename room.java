37148d4360e162593601a44471913e4c9150756c
Frankie <emcredmond@smith.edu>
2024-12-10 11:10:54 -0500

    update

--- room.java
+++ room.java
@@ -1,13 +1,18 @@
 public class Room {
+    String name;
     String clue;
+    String weapon;
 
-    public Room(){
-        String clue= " ";
+    public Room(String name, String clue, String weapon){
+        this.name= name;
+        this.clue= clue;
+        this.weapon= weapon;
     }
 
 
     public void lookAround(){
-        System.out.println("You found a Clue!!!! \n" + this.clue);
+        System.out.println("You found a Clue!!!! \n" + clue + "\n You also found a potential murder weapon!!!\n  the " + weapon+" does not have blood on it\n");
+        System.out.println("The object you found has been removed from your potential weapon list, would you like to remove any suspects ");
 
     }
 
